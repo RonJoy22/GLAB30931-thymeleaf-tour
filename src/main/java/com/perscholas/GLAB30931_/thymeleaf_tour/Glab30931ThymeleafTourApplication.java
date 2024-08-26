4bb139a9fbdafa26d5adb09c5b6f2377fe462af0
Joyner-Brown <ronald.joyner-brown@accenture.com>
2024-08-26 11:18:32 -0400

    Initial commit

---
+++ src/main/java/com/perscholas/GLAB30931_/thymeleaf_tour/Glab30931ThymeleafTourApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.GLAB30931_.thymeleaf_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab30931ThymeleafTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab30931ThymeleafTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perscholas/GLAB30931_/thymeleaf_tour/Glab30931ThymeleafTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.GLAB30931_.thymeleaf_tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab30931ThymeleafTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
